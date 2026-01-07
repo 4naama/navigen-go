43c74216ceb2a2aae2504f7cce1ea5a351835d71
4naama <4naama@gmail.com>
2026-01-07 20:16:07 +0100

    Frontend deploy

--- frontend/public/modal-injector.js
+++ frontend/public/modal-injector.js
@@ -3411,6 +3411,7 @@ export function showCampaignFundingModal({ locationID, campaignKey }) {
       <button type="button" class="campaign-funding-chip is-selected" data-eur="50">€50</button>
       <button type="button" class="campaign-funding-chip" data-eur="75">€75</button>
       <button type="button" class="campaign-funding-chip" data-eur="100">€100</button>
+      <button type="button" class="campaign-funding-chip" data-eur="150">€150</button>
     </div>
 
     <div class="campaign-funding-input-row">
