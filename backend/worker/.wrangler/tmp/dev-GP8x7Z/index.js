5cc28737154530e076ca3f43f5a95d122e295799
Your Name <USER@localhost>
2026-05-15 08:34:24 +0200

    Backend deploy

--- backend/worker/.wrangler/tmp/dev-GP8x7Z/index.js
+++ backend/worker/.wrangler/tmp/dev-GP8x7Z/index.js
@@ -2186,10 +2186,10 @@ var require_browser = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-JqMVrE/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-8SVySC/middleware-loader.entry.ts
 init_modules_watch_stub();
 
-// .wrangler/tmp/bundle-JqMVrE/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-8SVySC/middleware-insertion-facade.js
 init_modules_watch_stub();
 
 // src/index.ts
@@ -12843,7 +12843,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-JqMVrE/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-8SVySC/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -12876,7 +12876,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-JqMVrE/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-8SVySC/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
